dd6a2dcb2e69ea769b0cd348b397f7e35e07c377
Qian Ma <katharina.mq@outlook.de>
2022-10-25 10:23:44 +0200

    yayyy

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,31 +24,43 @@ namespace Spieleautomat
         {
             InitializeComponent();
 
-
-
-
         }
 
-        private static int Points()
-        {
-        
-        }
         private void bt1_Click(object sender, RoutedEventArgs e)
         {
             //spielen
 
-            lb1.Content = Random1();
-            lb2.Content = Random1();
-            lb3.Content = Random1();
+            int label1 = Random1();
+            int label2 = Random2();
+            int label3 = Random3();
+            lb1.Content = Converter(label1);
+            lb2.Content = Converter(label2);
+            lb3.Content = Converter(label3);
+
+            lb4.Content = Pointscalculation(label1,label2,label3);
         }
+
 
         static Random rng = new Random();
 
-        private static string Random1()
+        private static int Random1()
+        {
+            int random1 = rng.Next(2, 15);
+            return random1;
+        }
+        private static int Random2()
+        {
+            int random2 = rng.Next(2, 15);
+            return random2;
+        }
+        private static int Random3()
+        {
+            int random3 = rng.Next(2, 15);
+            return random3;
+        }
+        private static string Converter(int random)
         {
             string show = "";
-            int random = rng.Next(1, 15);
-
             if (random == 14)
             { show = "A"; }
             else if (random == 11)
@@ -57,12 +69,36 @@ namespace Spieleautomat
             { show = "Q"; }
             else if (random == 13)
             { show = "K"; }
-            
-            else {show = Convert.ToString(random);}
+            else { show = Convert.ToString(random); }
+
             return show;
-
         }
+        private static int Pointscalculation(int label1, int label2, int label3)
+        {
 
-
+            int points = 0;
+            
+            if (label1 == label2 && label2 == label3)
+            {
+                points = label1 * 100;
+            }
+            else
+            {
+                if (label1 == label2)
+                {
+                    points = (label1 * 10);
+                }
+                else if (label2 == label3)
+                {
+                    points = (label2 * 10);
+                }
+                else if (label1 == label3)
+                {
+                    points = (label1 * 10);
+                }
+                
+            }
+            return points;
+        }
     }
 }
